a65b4e7628764c0fe2fb685ad0e31bb4767294cd
fakebenjay <fakebenjay@gmail.com[color]>
2017-02-07 10:08:03 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
-  # code goes here
+  hash = {}
+
+  array.each do |string|
+    if hash.has_key?(string) == false
+      hash[string] = 1
+    else
+      hash[string] += 1
+    end
+  end
+
+  return hash
 end
- 
